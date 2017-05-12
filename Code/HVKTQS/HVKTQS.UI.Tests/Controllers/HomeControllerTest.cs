5060102cecead952cc39dc524a5c056e25e0614c
ManhLyTran <manhlytran@gmail.com>
2017-05-12 14:12:53 +0700

    clean code

--- Code/HVKTQS/HVKTQS.UI.Tests/Controllers/HomeControllerTest.cs
+++ Code/HVKTQS/HVKTQS.UI.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using HVKTQS.UI.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using HVKTQS.UI;
-using HVKTQS.UI.Controllers;
+using System.Web.Mvc;
 
 namespace HVKTQS.UI.Tests.Controllers
 {
